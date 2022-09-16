e46d0a5b030b5cb136cdaf59d7e320d8ab26d2b4
Fekete-Bruno <brunohffekete@gmail.com>
2022-09-16 05:13:46 -0300

    feat: GET product by Id

--- src/server.js
+++ src/server.js
@@ -3,10 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-app.get("/test", (req, res) => {
-  res.sendStatus(200);
-});
-
 app.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
 });
