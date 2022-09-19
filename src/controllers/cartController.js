e3854965dcfe8364a7e05ab356a6138514d4325e
Fekete-Bruno <brunohffekete@gmail.com>
2022-09-19 12:15:10 -0300

    fix delete headers

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,4 +1,5 @@
-import { ObjectId } from "mongodb";
+
+import { ObjectID } from "bson";
 import db from "../database/db.js";
 
 async function getCart(req,res){
@@ -26,9 +27,9 @@ async function addCart(req,res){
 }
 
 async function deleteCart(req,res){
-    const cartId = req.body._id;
+    const cartId = req.headers.data;
     try {
-        db.collection('carts').deleteOne({_id:ObjectId(cartId)});
+        await db.collection('carts').deleteOne({_id:ObjectID(cartId)});
     } catch (error) {
         console.error(error);
         return res.sendStatus(500);
